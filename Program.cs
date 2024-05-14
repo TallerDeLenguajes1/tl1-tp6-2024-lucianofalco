774ca7ff0fbd138c32b325625591d75f0e5fd7cf
“LucianoFalco” <lucianosebastianfalco@gmail.com>
2024-05-14 16:15:43 -0300

    ejercicio1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+Ejercicio 1. Construir un programa que permita invertir un número. Verifique que el
+texto ingresado es de hecho un número y, en caso afirmativo, realice la inversión del
+número sólo si éste es mayor a 0.
+Nota: Si observa un subrayado amarillo sugiriendo modificar las variables al tipo
+"nullable", abra el archivo de configuración de proyecto de extensión .csproj y elimine
+la línea que dice <Nullable>enable</Nullable>. No olvide guardar el archivo
+*/
 
-int a ;
-int b ;
-a = 10;
-b = a;
+using System;
 
-Console.WriteLine("El valor de a es : " + a);
-Console.WriteLine("El valor de b es : " + b);
+Console.WriteLine("Ingrese un numero");
+int numero =  Convert.ToInt32(Console.ReadLine());
+int inverso = 0;
+while (numero != 0)
+{
+    int digito = numero % 10 ;
+    inverso = inverso * 10 + digito ;
+    numero /= 10 ;
+}
+
+Console.WriteLine(inverso) ;
